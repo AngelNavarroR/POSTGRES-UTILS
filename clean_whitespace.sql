8968a192262d1c5c772f742cb73fadb6b7fd8de6
AngelNavarroR <48390911+AngelNavarroR@users.noreply.github.com>
2019-07-02 10:40:56 -0500

    Update clean_whitespace.sql

--- clean_whitespace.sql
+++ clean_whitespace.sql
@@ -1,5 +1,6 @@
 CREATE OR REPLACE FUNCTION clean_whitespace(to_clean text) RETURNS text AS $$
     BEGIN
+        to_clean = regexp_replace(to_clean, E'[ \t]+', ' ', 'g');
         RETURN regexp_replace(to_clean, E'[ tnr]+', ' ', 'g');
     END;
 $$ LANGUAGE plpgsql IMMUTABLE;
